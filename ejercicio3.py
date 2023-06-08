ea015846a4400b72c668f94e8ab6c9045b86feeb
Nacho Jimenez <ignaciokaram@gmail.com>
2023-06-08 12:14:08 -0300

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,12 @@
+# //* Hacer un programa que permita decidir si dos palabras son iguales o
+# diferentes. Mostrar mensaje por pantalla.*//
+
+palabra1 = input("Ingrese la primera palabra: ")
+palabra2 = input("Ingrese la segunda palabra: ")
+
+#verificar si son iguales
+
+if palabra1 == palabra2:
+    print("Las palabras ingresadas son iguales ")
+else:
+    print("Las palabaras no son iguales lo siento")
