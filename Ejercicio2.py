0e8e3f41d54e2d66b866b5daa2bd565e219962a7
Nacho Jimenez <ignaciokaram@gmail.com>
2023-06-08 12:12:56 -0300

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario dos letras
+letra1 = input("Ingrese la primera letra: ")
+letra2 = input("Ingrese la segunda letra: ")
+
+# Verificar si las letras son iguales
+if letra1 == letra2:
+    print("Las letras son iguales")
+else:
+    print("Las letras son diferentes")
