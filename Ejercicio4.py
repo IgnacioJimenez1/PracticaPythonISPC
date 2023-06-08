6e80705c6ff6d1db6e3cc3298672e4f19c365062
Nacho Jimenez <ignaciokaram@gmail.com>
2023-06-08 12:14:31 -0300

    Create Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,10 @@
+#Realice un programa que lea dos números y diga cuál es el mayor.
+
+
+numero1 = float(input("Ingrese la primera cantidad: "))
+numero2 = float(input("Ingrese la segunda cantidad: "))
+
+if numero1 > numero2:
+    print ("El numero 1 es mayuor que el numero 2")
+else:
+    print("El numero 2 es mayor que el numero 1")
