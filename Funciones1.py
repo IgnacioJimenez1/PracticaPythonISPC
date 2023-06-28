79b4b407b3b4780556dd50e15a1036147103d3ac
Ignacio Jimenez <ignaciokaram@gmail.com>
2023-06-28 02:07:38 -0300

    comentario del ejercicio y definimos la función

---
+++ Funciones1.py
@@ -0,0 +1,2 @@
+#Realice un programa que muestre el mensaje “Hola Aula X (Indicar el número de aula a la que pertenecen),
+# ¿Qué tal?” en tres veces intercambiados entre ellos otro mensajes a su elección. 
