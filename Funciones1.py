fa7c8c0c8aa059ce0b4a3f43d2c91e7f91a211bb
Ignacio Jimenez <ignaciokaram@gmail.com>
2023-06-28 02:09:47 -0300

    Update Funciones1.py

--- Funciones1.py
+++ Funciones1.py
@@ -1,2 +1,25 @@
-#Realice un programa que muestre el mensaje “Hola Aula X (Indicar el número de aula a la que pertenecen),
+# 1. Realice un programa que muestre el mensaje “Hola Aula X (Indicar el número de aula a la que pertenecen),
 # ¿Qué tal?” en tres veces intercambiados entre ellos otro mensajes a su elección. 
+
+
+def intercambiar_mensajes(aula):
+    print(f"Hola Aula {aula}, ¿Qué tal?")
+
+
+    # codigo donde se realiza el intercambio de mensajes
+    mensaje1 = input("Ingresa el primer mensaje: ")
+    mensaje2 = input("Ingresa el segundo mensaje: ")
+    mensaje3 = input("Ingresa el tercer mensaje: ")
+
+    print(f"Aula {aula} dice: {mensaje3}")
+    print(f"Aula {aula + 1} dice: {mensaje2}")
+    print(f"Aula {aula + 2} dice: {mensaje1}")
+
+
+    # Solicitar número de aula al usuario
+    num_aula = int(input("Ingresa el número de aula: "))
+
+    # Mostrar el mensaje y realizar el intercambio de mensajes tres veces
+    for _ in range(3):
+        intercambiar_mensajes(num_aula)
+        num_aula += 3
